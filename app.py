d4db0daeb7778186cc3b3138da67f63da8c0162f
mikl2 <sunflowerlmao@vk.com>
2024-05-29 23:40:11 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import time
 import random
-from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_file
 import os
 import zipfile
 import webbrowser
@@ -10,8 +10,6 @@ import webbrowser
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
-
-f = open('logfile.txt', 'w')
 selected_grafs = []
 bd = []
 zadach = ['', '']
@@ -19,7 +17,12 @@ timetest = []
 logtext = []
 k = 0
 submitier = [""]
-ierog_files = [f for f in os.listdir('static/ierog') if os.path.isfile(os.path.join('static/ierog', f))]
+ierog_files = []
+start_time = 0
+running = False
+filenum = 0
+timeofstart = time.localtime(time.time())
+formtime = time.strftime("%H.%M.%S", timeofstart)
 
 
 def bdload():
@@ -40,7 +43,6 @@ def foundindex(a):
     for j in range(0,len(zadach[1])):
         zadach[1][j] += '.png'
 
-
 def selfile(a):
     cor = []
     for i in range(len(a)):
@@ -49,7 +51,6 @@ def selfile(a):
 
 def submitgrafems():
     zadacha = list(zadach[1])
-    print(zadacha)
     s = sorted(selected_grafs)
     z = sorted(zadacha)
     count = 0
@@ -81,56 +82,64 @@ def sortir(a):
     else:
         return [f for f in os.listdir('static/graf') if os.path.isfile(os.path.join('static/graf', f))]
 
-
+def averagetime(f):
+    temp = ['\n','Количество иероглифов: ','\n','Среднее время ввода иероглифа: ','\n']
+    temp.insert(2,str(len(timetest)))
+    tempt = str(sum(timetest)/len(timetest))
+    temp.insert(5,tempt)
+    return temp
 def logfilewrite(f):
     logfile = open('logfile.txt', 'w')
-    n = 0
-    if n == 0:
-        logfile.write("Эксперемент начался!\n")
-        n += 1
+    logfile.truncate()
     logfile.writelines(f)
+    if len(timetest) > 0:
+        logfile.writelines(averagetime(f))
+    logfile.writelines('Время проведения эксперимента - '+str(formtime))
     logfile.close()
 
 def randomier():
-    global ierog_files
-    if not ierog_files:
-        ierog_files = [f for f in os.listdir('static/ierog') if os.path.isfile(os.path.join('static/ierog', f))]
-    file = random.choice(ierog_files)
-    ierog_files.remove(file)
+    global ierog_files,filenum
+    if os.path.isfile('uploads/shablon.txt'):
+        shablon = open('uploads/shablon.txt','r')
+        if len(ierog_files) == 0:
+            for line in shablon:
+                ierog_files.append(str(line.strip())+'.bmp')
+        file = ierog_files[filenum]
+        filenum += 1
+    else:
+        if len(ierog_files) == 0:
+            ierog_files = [f for f in os.listdir('static/ierog') if os.path.isfile(os.path.join('static/ierog', f))]
+        file = random.choice(ierog_files)
+        ierog_files.remove(file)
     return file
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 @app.route('/test', methods=['GET', 'POST'])
 def test():
-    global selected_grafs,k,getier
+    global selected_grafs,k
     bdload()
+    logfilewrite(logtext)
     if len(selected_grafs) > 0:
         folder1_images = sortir(selected_grafs)
     else:
         folder1_images = [f for f in os.listdir('static/graf') if os.path.isfile(os.path.join('static/graf', f))]
-    print(submitier)
+
     if submitier[-1] == "1":
         submitier.append("0")
         folder2_image = randomier()
-        print(111111222,folder2_image)
         foundindex(folder2_image)
         return render_template('test.html', folder1_images=folder1_images, folder2_image=folder2_image,
                                selected_grafs=selected_grafs)
-
-    elif k == 0:
-        folder2_image = randomier()
-        print(111111000, randomier())
-        foundindex(folder2_image)
-        k += 1
     else:
-        folder2_image = zadach[0] + '.bmp'
+        if k == 0:
+            folder2_image = randomier()
+            foundindex(folder2_image)
+            k += 1
+        else:
+            folder2_image = zadach[0] + '.bmp'
     return render_template('test.html', folder1_images=folder1_images, folder2_image=folder2_image,
                            selected_grafs=selected_grafs)
-
 @app.route('/set_remove', methods=['POST'])
 def set_remove():
     global isremove
@@ -140,11 +149,12 @@ def set_remove():
 
 @app.route('/submit_selected_grafems', methods=['POST'])
 def submit_selected_grafems():
-    print('123123')
+    global elapsed_time
     if submitgrafems() == 1:
         selected_grafs.clear()
         submitier.append("1")
-
+        logtext.append(str(zadach[0]) +':'+ str(int(elapsed_time // 3600))+'.'+str(int((elapsed_time % 3600) // 60))+'.'+str(int(elapsed_time % 60))+'\n')
+        timetest.append(int(str(elapsed_time).split('.')[0]))
         return jsonify({'result': 'correct'})
     else:
         submitier.append("0")
@@ -161,7 +171,6 @@ def select_image():
     else:
         selected_grafs.append(image_name)
         print('add', selected_grafs)
-    print('zadach', zadach)
     return jsonify({'selected_grafs': selected_grafs})
 
 
@@ -170,20 +179,32 @@ def update_selected_grafs():
     selected_grafs = request.form['selected_grafs']
     selected_grafs = json.loads(selected_grafs)
     return jsonify({'selected_grafs': selected_grafs})
+@app.route("/start", methods=["POST"])
+def start_timer():
+    global start_time, running,k
+    if k == 1:
+        start_time = time.time()
+        running = True
+        k += 1
+        return "Timer started"
+@app.route("/stop", methods=["POST"])
+def stop_timer():
+    global running
+    running = False
+    return "Timer stopped"
+@app.route("/time", methods=["GET"])
+def get_time():
+    global start_time, running, elapsed_time
+    if running:
+        elapsed_time = time.time() - start_time
+        return f"{int(elapsed_time // 3600):02d}:{int((elapsed_time % 3600) // 60):02d}:{int(elapsed_time % 60):02d}"
+    else:
+        return "Эксперимент остановлен!"
 
-@app.route('/update_timer', methods=['POST'])
-def update_timer():
-    hours = request.form['hours']
-    minutes = request.form['minutes']
-    seconds = request.form['seconds']
-    timess = str(hours)+':'+str(minutes)+":"+str(seconds)
-    timetest.append(timess)
-    print('times',timess)
-    # Do something with the timerElapsed variable here
-    return jsonify({'status': 'success'})
-
-
-@app.route('/download', methods=['GET', 'POST'])
+@app.route('/download', methods=['POST'])
+def download_file():
+    return send_file('logfile.txt', as_attachment=True)
+@app.route('/upload', methods=['GET', 'POST'])
 def download():
     if request.method == 'POST' and request.files:
         graf = request.files["graf"]
@@ -220,7 +241,7 @@ def download():
             shablon.save(os.path.join(app.config['UPLOAD_FOLDER'], shablon.filename))
         return redirect(url_for('success'))
 
-    return render_template('download.html')
+    return render_template('upload.html')
 
 
 @app.route('/success', methods=['GET', 'POST'])
